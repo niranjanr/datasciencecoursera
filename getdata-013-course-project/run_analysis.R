fb9a4ce9f4c31d01c0a55261e9ea32c79259a6af
Niranjan Ravichandran <niranjan@asana.com>
2015-04-25 21:15:51 -0700

    Clean up variable names and the global environment in run_analysis.R

--- getdata-013-course-project/run_analysis.R
+++ getdata-013-course-project/run_analysis.R
@@ -1,25 +1,39 @@
+# Assumptions:
+# 1) The current working directory consists of all the files that are present in
+#     the "UCI HAR Dataset" folder when you unzip
+#     https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+# 2) List of files:
+#   a) features.txt
+#   b) activity_labels.txt
+#   c) test/X_test.txt
+#   d) test/y_test.txt
+#   e) test/subject_test.txt
+#   f) train/X_train.txt
+#   g) train/y_train.txt
+#   h) train/subject_train.txt
+
 library(dplyr)
 
-features <- read.table("UCI HAR Dataset/features.txt",
+features <- read.table("features.txt",
                        header = FALSE,
                        stringsAsFactors = FALSE)
 
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt",
+activity_labels <- read.table("activity_labels.txt",
                        header = FALSE,
                        stringsAsFactors = FALSE)
 
 # read in test data
-data_X_test <- read.table("UCI HAR Dataset/test/X_test.txt",
+data_X_test <- read.table("test/X_test.txt",
                           header = FALSE)
-data_y_test <- read.table("UCI HAR Dataset/test/y_test.txt",
+data_y_test <- read.table("test/y_test.txt",
                           header = FALSE)
-data_subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt",
+data_subject_test <- read.table("test/subject_test.txt",
                                 header = FALSE)
 
 # read in training data
-data_X_train <- read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
-data_y_train <- read.table("UCI HAR Dataset/train/y_train.txt", header = FALSE)
-data_subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt",
+data_X_train <- read.table("train/X_train.txt", header = FALSE)
+data_y_train <- read.table("train/y_train.txt", header = FALSE)
+data_subject_train <- read.table("train/subject_train.txt",
                                  header = FALSE)
 
 # merge the training and test data sets along with the activity information
@@ -29,15 +43,15 @@ combined_dataset <- rbind(data_X_test, data_X_train)
 colnames(combined_dataset) <- features$V2
 
 # extract measurements only on the mean and standard deviation
-column_filtered_dataset <-
+step4_dataset <-
   combined_dataset[, grep("-mean|-std",
                           colnames(combined_dataset),
                           ignore.case = TRUE,
                           value = TRUE)]
 
 # combine Y data & subject t
-column_filtered_dataset$activityLevel <- (rbind(data_y_test, data_y_train))$V1
-column_filtered_dataset$subject <- (rbind(data_subject_test, data_subject_train))$V1
+step4_dataset$activityLevel <- (rbind(data_y_test, data_y_train))$V1
+step4_dataset$subject <- (rbind(data_subject_test, data_subject_train))$V1
 
 # clean up the workspace and remove environment variables that we no longer need
 rm (data_X_test)
@@ -49,14 +63,15 @@ rm (data_subject_train)
 rm (combined_dataset)
 
 # replace the activity levels with the text / factors
-column_filtered_dataset <- mutate(column_filtered_dataset,
+step4_dataset <- mutate(step4_dataset,
                                   activityLevel = activity_labels$V2[activityLevel])
 
 # create another tidy data set that has the mean of different features grouped
 # by activity levels for each subject
-tidy_data_set <- column_filtered_dataset %>%
+step5_dataset <- step4_dataset %>%
   group_by(subject, activityLevel) %>%
   summarise_each(funs(mean))
+step5_dataset
 
 # final clean up
 rm (activity_labels)
